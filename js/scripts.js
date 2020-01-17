c1d501d6489e450cf0af501052dff0358d4647f3
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-16 19:38:34 -0800

    reorganize if else

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,3 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
-
-
-
-if (side1 === side2 && side1 === side3) {
-  alert("Equilateral");
-} else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
-  alert ("That's not a triangle!");
-} else if (side1 === side2 || side1 === side3 || side2 === side3) {
